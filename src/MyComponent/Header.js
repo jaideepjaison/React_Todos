9bd5117be63388c8fa5306289afe6d512cb75e99
jaideepjaison <jaison3666@gmail.com>
2023-05-23 21:45:22 +0530

    new branch by jaison

--- src/MyComponent/Header.js
+++ src/MyComponent/Header.js
@@ -1,10 +1,10 @@
 import React from 'react'
-
+//hi
 export default function Header() {
   return (
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">MyApp</a>
+    <a class="navbar-brand" href="#jaison_branch">MyApp</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
